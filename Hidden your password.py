9d005032b43c75c6964606c40eb43fee7f4193b8
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-08-11 21:44:07 +0530

    Add files via upload

---
+++ Hidden your password.py
@@ -0,0 +1,7 @@
+Username= input("What is your Username")
+Password= input("What is your Password")
+
+Passsword_length= len(password)
+hidden_password= '*' * password_length
+
+print(f '{Username},your password, {hidden_password}, is{password_length} letters long')
